6330dd8adb2bdad1e8ed2b9520046936e65fc1a3
budiwi24 <budi.wiatna@ralali.com>
2019-07-03 16:06:26 +0700

    add tc failed reg with wrong format email

--- api-rl/api/test/register-mobile-test.js
+++ api-rl/api/test/register-mobile-test.js
@@ -7,8 +7,11 @@ const testCase = {
  "positive" : {
     "RegisNewMember" : "As a New User, I want to register for login",
  },
- "negative" : {
-    "RegWithEmptyEmail" : "User registration with empty email"
+ "negative1" : {
+    "RegWithSameEmail" : "User registration with same email"
+ },
+ "negative2" : {
+    "RegWithInvalidEmail" : "User registration with invalid email format"
  }
 }
 
@@ -20,9 +23,16 @@ describe(`Register Mobile Apps`, () => {
     assert(response.status).to.equal(400);
    }),
 
-it (`post ${testCase.negative.RegWithEmptyEmail}`, async() => {
-   const response = await page.RegisterMobile(data.invalid);
-   assert(response.status).to.equal(200);
+it (`@post ${testCase.negative1.RegWithSameEmail}`, async() => {
+   const response = await page.RegisterMobile(data.sameemail);
+   assert(response.status).to.equal(400);
+   assert(response.body.message).to.equal('Please confirm email');
+   }),
+
+it (`@post ${testCase.negative2.RegWithInvalidEmail}`, async() => {
+   const response = await page.RegisterMobile(data.wrongformatemail)
+   assert(response.status).to.equal(400);
+   assert(response.body.message).to.equal('Failed to register, please contact our customer service')
 })
 
 }) 
