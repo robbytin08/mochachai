4a63b0587cc1239d17c533b5c363b759b741b11a
budiwi24 <budi.wiatna@ralali.com>
2019-07-03 13:38:16 +0700

    commit

---
+++ api-rl/api/page/login-mobile-page.js
@@ -0,0 +1,15 @@
+const supertest = require('supertest');
+const env = require('dotenv').config();
+
+const api = supertest(process.env.RLL_BASE_URL);
+
+const LoginMobile = (login) => api.post('/api/mobile/v1/user/register')
+ .set('Content-Type', 'application/json')
+ .set('x-key', '20151020052357')
+ .set('x-secret','75325002011502')
+ .send(login);
+// .query({email : Email, password : Pwd, name : Username, subscribe_news : Subscribe})
+
+module.exports = {
+    RegisterMobile,
+}
--- api-rl/api/test/register-mobile-test.js
+++ api-rl/api/test/register-mobile-test.js
@@ -7,13 +7,22 @@ const testCase = {
  "positive" : {
     "RegisNewMember" : "As a New User, I want to register for login",
  },
+ "negative" : {
+    "RegWithEmptyEmail" : "User registration with empty email"
+ }
 }
 
 describe(`Register Mobile Apps`, () => {
 
  it(`@post ${testCase.positive.RegisNewMember}`, async() => {
    const response = await page.RegisterMobile(data.valid);
-    assert(response.status).to.equal(200);
-   })
+   //  assert(response.status).to.equal(200);
+    assert(response.status).to.equal(400);
+   }),
+
+it (`post ${testCase.negative.RegWithEmptyEmail}`, async() => {
+   const response = await page.RegisterMobile(data.invalid);
+   assert(response.status).to.equal(200);
+})
 
 }) 
